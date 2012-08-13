6827354efdd50e0e1812273ba3db98d67e72936e
Micah Buuck <buuck@stolaf.edu>
2012-08-13 12:18:44 -0700

    plot_transfer_data.py
    function for plotting data saved by power_scan.py script

---
+++ plot_transfer_data.py
@@ -0,0 +1,28 @@
+def plot_transfer_data(filename):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from matplotlib import cm
+    from mpl_toolkits.mplot3d import Axes3D
+    
+    f = open(filename,'r')
+    data = eval(f.read())
+    f.close()
+    x = []
+    y = []
+    z = []
+    for i in data:
+        x.append(i[0])
+        y.append(i[1])
+        z.append(i[2])
+
+    x = np.array(x)
+    y = np.array(y)
+    z = np.array(z)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    #plt.xlabel('HF Peak (MHz)')
+    #plt.ylabel('LO Frequency (MHz)')
+    #plt.zlabel('Power (dBm)')
+    plt.title('Transfer Function of Receiver Chain')
+    surf = ax.plot_surface(x,y,z,rstride=5,cstride=25,cmap=cm.jet)
+    plt.show()
