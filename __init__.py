4a96ad35d5629d17175840517ca67615743082a6
Ben LaRoque <benjamin.laroque@gmail.com>
2012-08-21 22:46:23 -0700

    Import of some components is now conditional. Basic command database usage is supported with minimal dependencies.

--- __init__.py
+++ __init__.py
@@ -4,5 +4,11 @@
 
 from DripInterface import DripInterface
 from DripResponse import DripResponse
-from peakdet import peakdet
-from LoggedDataHandler import LoggedDataHandler
+try:
+    from peakdet import peakdet
+except ImportError:
+    print('import of peakdet failed, some dependency must be missing')
+try:
+    from LoggedDataHandler import LoggedDataHandler
+except ImportError:
+    print('import of LoggedDataHandler failed, it is not available')
