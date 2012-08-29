bd6f4488979911cac1dd127ec4a0a5c13e11aa13
Ben LaRoque <benjamin.laroque@gmail.com>
2012-08-29 15:41:22 -0700

    Fixing python 3 compatibility, should work on python 2 and python 3 now.

--- Pypeline/__init__.py
+++ Pypeline/__init__.py
@@ -1,14 +1,29 @@
 '''
     Package init for pypeline. For now pypeline.py containing the pypeline class is all there is so not much here.
 '''
+from os import environ
 
-from DripInterface import DripInterface
-from DripResponse import DripResponse
 try:
-    from peakdet import peakdet
+    from .DripInterface import DripInterface
 except ImportError:
-    print('import of peakdet failed, some dependency must be missing')
+    import DripInterface.DripInterface
 try:
-    from LoggedDataHandler import LoggedDataHandler
+    from .DripResponse import DripResponse
 except ImportError:
-    print('import of LoggedDataHandler failed, it is not available')
+    from DripResponse import DripResponse
+if "DISPLAY" in environ:
+    if environ["Display"]:
+        try:
+            from . import peakdet
+        except ImportError:
+            try:
+                from peakdet import peakdet
+            except ImportError:
+                print('import of peakdet failed, some dependency must be missing')
+        try:
+            from . import LoggedDataHandler
+        except ImportError:
+            try:
+                from LoggedDataHandler import LoggedDataHandler
+            except ImportError:
+                print('import of LoggedDataHandler failed, it is not available')
