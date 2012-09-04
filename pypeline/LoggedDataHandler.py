64897b6d9f76d537b25acfe527e3580035d4dfdf
Micah Buuck <buuck@stolaf.edu>
2012-09-04 12:35:54 -0700

    Cleaned up code a little

--- pypeline/LoggedDataHandler.py
+++ pypeline/LoggedDataHandler.py
@@ -97,47 +97,27 @@ class LoggedDataHandler:
             for sensor in sensors:
                 data = self.Get(sensor, start, stop)
                 line1 = ax.plot(data[0][sensor], data[1][sensor], label=sensor)
-                fig.autofmt_xdate()
 
             while dynamupdate:
                 try:
                     ax.clear()
                     for sensor in sensors:
                         data = self.Get(sensor, start, stop)
-                        line1 = ax.plot(data[0][sensor], data[1][sensor], label=sensor)
-                    fig.autofmt_xdate()
-                    plt.xlabel('Time (Hours)')
-                    plt.ylabel('Value (' + data[2][sensor][0] + ')')
-                    plt.title('Sensor Readout')
-                    box = ax.get_position()
-                    ax.set_position([box.x0, box.y0 + box.height * 0.1,
-                                     box.width, box.height * 0.9])
-                    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),
-                              fancybox=True, shadow=True, ncol=2)
+                        line1 = ax.plot(data[0][sensor], data[1][sensor],
+                                        label=sensor)
+                    self.FormatPlots(sensor,fig,ax)
                     fig.canvas.draw()
                 except KeyboardInterrupt:
                     ax.clear()
                     for sensor in sensors:
                         data = self.Get(sensor, start, stop)
-                        line1 = ax.plot(data[0][sensor], data[1][sensor], label=sensor)
-                    fig.autofmt_xdate()
-                    plt.xlabel('Time (Hours)')
-                    plt.ylabel('Value (' + data[2][sensor][0] + ')')
-                    plt.title('Sensor Readout')
-                    box = ax.get_position()
-                    ax.set_position([box.x0, box.y0 + box.height * 0.1,
-                                     box.width, box.height * 0.9])
-                    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),
-                              fancybox=True, shadow=True, ncol=2)
+                        line1 = ax.plot(data[0][sensor], data[1][sensor],
+                                        label=sensor)
+                    self.FormatPlots(sensor,fig,ax)
                     fig.canvas.draw()
                     break
             if not dynamupdate:
-                plt.xlabel('Time (Hours)')
-                plt.ylabel('Value (' + data[2][sensor][0] + ')')
-                plt.title('Sensor Readout')
-                box = ax.get_position()
-                ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-                ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+                self.FormatPlots(sensor,fig,ax)
                 plt.show()
 
     def Save(self, filename):
@@ -181,7 +161,11 @@ class LoggedDataHandler:
         x = np.array(x)
         y = np.array(self.values[sensor])
         p1, success = optimize.leastsq(errfunc, p0[:], args=(x, y))
-        plt.plot(x, y, "bo", x, fitfunc(p1, x), "r-")
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        points = plt.plot(x, y, "bo", label=sensor)
+        fit = plt.plot(x, fitfunc(p1, x), "r-", label='fit')
+        self.FormatPlots(sensor,fig,ax)
         print(p1)
         plt.show()
 
@@ -194,3 +178,17 @@ class LoggedDataHandler:
             if row.value['sensor_name'] not in sensors:
                 sensors.append(row.value['sensor_name'])
         print(sensors)
+
+    def FormatPlots(self,sensor,fig,ax):
+        '''
+            Internal
+        '''
+        fig.autofmt_xdate()
+        plt.xlabel('Time (Hours)')
+        plt.ylabel('Value (' + self.units[sensor][0] + ')')
+        plt.title('Sensor Readout')
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                         box.width, box.height * 0.9])
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),
+                  fancybox=True, shadow=True, ncol=2)
