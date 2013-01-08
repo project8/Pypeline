a59b0ef8f0e5524a489b0cfbb3b49b001e4bdd87
Ben LaRoque <benjamin.laroque@gmail.com>
2013-01-07 16:31:37 -0800

    Creating a script to plot two slow control channels against each other.

---
+++ scripts/channel_vs_channel.py
@@ -0,0 +1,32 @@
+'''
+    Utility script which will plot logger data for two channels, one against the other.
+
+    Per usual, the module can be can be imported and the function used in some other module, or called directly from the terminal with arguments unpacked into the arguments in order.
+'''
+
+# standard libraries
+# third party libraries
+# pypeline and 'internal' libraries
+import pypeline
+
+def channel_vs_channel(channely, channelx, start, stop):
+    '''
+        Utility function which will plot logger data for two channels, one against the other.
+        <channely> is the channel name for y values (a string)
+        <channelx> is the channel name for x values (a string)
+        <start> is the start time for the plot (a datetime object)
+        <stop> is the stop time for the plot (a datetime object)
+    '''
+    ldh = pypeline.LoggedDataHandler('http://p8portal.phys.washington.edu:5984')
+    string_format = '%Y-%m-%d %H:%M:%S'
+    xdata = ldh.Get(channelx, start.strftime(string_format), stop.strftime(string_format))
+    ydata = ldh.Get(channely, start.strftime(string_format), stop.strftime(string_format))
+
+
+if __name__=='__main__':
+    try:
+        channel_vs_channel(*sys.argv[1:])
+    except:
+        print("it didn't work, I'll put in better exception catching later")
+        print("probably bad arguments... you gave this:")
+        print(sys.argv[1:])
