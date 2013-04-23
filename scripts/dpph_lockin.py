e00324e6b5ced320c7d8b1d4f8fa1182eeec1f01
Ben LaRoque <benjamin.laroque@gmail.com>
2013-04-23 11:22:57 -0700

    More debugs, it just worked twice

--- scripts/dpph_lockin.py
+++ scripts/dpph_lockin.py
@@ -2,7 +2,7 @@
 from time import sleep
 from sys import stdout
 # 3rd party
-from numpy import std, mean, array, less, arange
+from numpy import std, mean, array, less, arange, pi
 from scipy import stats, signal
 # local
 from pypeline import DripInterface, usegnuplot
@@ -36,6 +36,17 @@ def GetLockinValue(interface, freq=25553.440, power=-40, slptime=1):
         else:
             raise
 
+def GetVoltages(freq_list, reference=0, deviation=0.2, stop_condition=1e10):
+    '''
+        Get a list for frequency <-> lockin voltage pairs with updates to the user
+
+        <freq_list>:    an iterable of frequencies in MHz
+        <reference>:    if stopping at structure, this is the reference voltage (usually mean)
+        <deviation>:    if stopping at structure, count number of these away (usually standard deviation)
+        <stop_condition>:   number of <deviation> from <reference> to stop looping
+    '''
+    pass
+
 if __name__ == "__main__":
     '''
         If run as: $python dpph_lockin.py, do these things:
@@ -43,8 +54,8 @@ if __name__ == "__main__":
     num_stats_freqs = 5
     pype = DripInterface('http://p8portal.phys.washington.edu:5984')
 
-#    freqs = range(25000, 26500, 10)
-    freqs = range(25640, 26500, 10)
+    freqs = range(25000, 26500, 10)
+#    freqs = range(25640, 26500, 10)
     
     #determine a mean and standard deviation
     VDC = [GetLockinValue(pype, freq) for freq in freqs[0:num_stats_freqs]]
@@ -71,7 +82,7 @@ if __name__ == "__main__":
     #take a set of fine data points to capture the structure
     try:
         assert interesting_freq, 'interesting_freq'
-        fine_freqs = range(interesting_freq-15, interesting_freq+25)
+        fine_freqs = range(interesting_freq-20, interesting_freq+30, 2)
         VDC_fine = []
         #VDC = VDC + [GetLockinValue(pype, freq) for freq in fine_freqs]
         VDC_fine = [GetLockinValue(pype, freq) for freq in fine_freqs]
@@ -80,20 +91,22 @@ if __name__ == "__main__":
         #    print(str(freq) + ' MHz -> ' + str(volt) + ' V')
         min_index = VDC_fine.index(min(VDC_fine))
         max_index = VDC_fine.index(max(VDC_fine))
-        minima = signal.argrelextrema(array(VDC_fine), less)[0]
-        min_index = max((array(VDC_fine)[minima]<0)*minima)
-        maxima = signal.argrelextrema(array(VDC_fine), less)[0]
-        max_index = min
+        #minima = signal.argrelextrema(array(VDC_fine), less)[0]
+        #min_index = min((array(VDC_fine)[minima]<0)*minima)
+        #maxima = signal.argrelextrema(array(VDC_fine), less)[0]
+        #max_index = max((array(VDC_fine)[maxima]>0)*maxima)
 
         #take some very finely spaced data for doing a fit
-        glmin = VDC_fine.index(min(VDC_fine))
-        glmax = VDC_fine.index(max(VDC_fine))
-        firstpos = list(array(VDC_fine[glmin:glmax])>0).index(True)+glmin
-        very_fine_freqs = arange(fine_freqs[firstpos-2], fine_freqs[firstpos+1], 0.2)
+        glmin = VDC_fine.index(max(VDC_fine))
+        glmax = VDC_fine.index(min(VDC_fine))
+        print('max and min are: ' + str(max(VDC_fine)) + ' ' + str(min(VDC_fine)))
+        firstpos = list(array(VDC_fine[glmin:glmax])<0).index(True)+glmin
+        very_fine_freqs = arange(fine_freqs[firstpos-1], fine_freqs[firstpos], 0.1)
         print('starting very fine grain frequency measurement')
-        VDC_very_fine = [GetLockinValues(pype, freq) for freq in very_fine_freq]
+        VDC_very_fine = [GetLockinValue(pype, freq) for freq in very_fine_freqs]
 
         slope, intercept, r_value, p_value, std_err = stats.linregress(array(very_fine_freqs), array(VDC_very_fine))
+        
 
         dataset = zip(fine_freqs+list(very_fine_freqs),VDC_fine+VDC_very_fine)
         fitline = [fine_freqs[min_index], slope*fine_freqs[min_index]+intercept,
@@ -111,8 +124,18 @@ if __name__ == "__main__":
         plot.gp("set ylabel \"Hall Probe\"")
         plot.gp("unset key")
         plot.g.stdin.write('set arrow from ' + str(fitline[0]) +','+ str(fitline[1]) + ' to ' + str(fitline[2]) +',' + str(fitline[3]) + 'nohead\n')
-        plot.plot1d(dataset, ' with lines')
-        raw_input('Found zero crossing at ' + str(-intercept/slope) + '\nwaiting for you to finish looking')
+        plot.plot1d(dataset, '')
+
+        print('Found zero crossing at ' + str(-intercept/slope))
+        geff = 2.0036
+        chargemass = 1.758e11
+        print('find found slope: '+str(slope)+
+              'intercept: '+str(intercept)+
+              'r_value: '+str(r_value)+
+              'p_value: '+str(p_value)+
+              'std_err: '+str(std_err))
+        print('Field is: ' + str(4*pi*(-intercept/slope)*10**7/(geff*chargemass)) + ' kGauss')
+        raw_input('waiting for you to finish looking')
 
     except AssertionError as e:
         if e[0] == 'interesting_freq':
