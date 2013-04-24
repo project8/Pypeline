33f30aad414434fa02ee41f86a3682379e33556b
Ben LaRoque <benjamin.laroque@gmail.com>
2013-04-24 12:20:54 -0700

    fixing polarity dependence

--- scripts/dpph_lockin.py
+++ scripts/dpph_lockin.py
@@ -2,12 +2,12 @@
 from time import sleep
 from sys import stdout
 # 3rd party
-from numpy import std, mean, array, less, arange, pi
+from numpy import std, mean, array, less, arange, pi, where, diff, sign
 from scipy import stats, signal
 # local
 from pypeline import DripInterface, usegnuplot
 
-def GetLockinValue(interface, freq=25553.440, power=-40, slptime=1):
+def GetLockinValue(interface, freq=25553.440, power=-60, slptime=1):
     '''
         Make a reading with the lockin amplifier at a specific frequency.
 
@@ -36,16 +36,25 @@ def GetLockinValue(interface, freq=25553.440, power=-40, slptime=1):
         else:
             raise
 
-def GetVoltages(freq_list, reference=0, deviation=0.2, stop_condition=1e10):
+def GetVoltages(pype, freq_list, power=-40, reference=0, deviation=0.2, stop_condition=1e10):
     '''
         Get a list for frequency <-> lockin voltage pairs with updates to the user
 
+        <pype>:         pypeline DripInterface instance
         <freq_list>:    an iterable of frequencies in MHz
         <reference>:    if stopping at structure, this is the reference voltage (usually mean)
         <deviation>:    if stopping at structure, count number of these away (usually standard deviation)
         <stop_condition>:   number of <deviation> from <reference> to stop looping
     '''
-    pass
+    VDC = []
+    for freq in freq_list:
+        stdout.write('trying ' + str(freq) + ' MHz\r')
+        stdout.flush()
+        VDC.append(GetLockinValue(pype, freq, power))
+        if abs((VDC[-1]-reference)/deviation) > stop_condition:
+            print('something of interest at ' + str(interesting_freq) + ' MHz')
+            break
+    return VDC
 
 if __name__ == "__main__":
     '''
@@ -55,7 +64,6 @@ if __name__ == "__main__":
     pype = DripInterface('http://p8portal.phys.washington.edu:5984')
 
     freqs = range(25000, 26500, 10)
-#    freqs = range(25640, 26500, 10)
     
     #determine a mean and standard deviation
     VDC = [GetLockinValue(pype, freq) for freq in freqs[0:num_stats_freqs]]
@@ -68,42 +76,51 @@ if __name__ == "__main__":
 
     #find where the structure starts
     interesting_freq = False
-    for freq in freqs[num_stats_freqs:]:
-        #print('trying ' + str(freq) + ' MHz')
-        stdout.write('trying ' + str(freq) + ' MHz\r')
-        stdout.flush()
-        VDC.append(GetLockinValue(pype, freq))
-        VDC_freqs.append(freq)
-        if abs((VDC[-1]-VDC_mean)/VDC_std) > 40:
-            interesting_freq = freq
-            print('something of interest at ' + str(interesting_freq) + ' MHz')
-            break
+    VDC += GetVoltages(pype, freqs[num_stats_freqs:],
+                       reference=VDC_mean, deviation=VDC_std, stop_condition=40)
+    VDC_freqs = freqs[:len(VDC)]
+    if not len(VDC) == len(freqs):
+        interesting_freq = VDC_freqs[-1]
 
     #take a set of fine data points to capture the structure
     try:
         assert interesting_freq, 'interesting_freq'
         fine_freqs = range(interesting_freq-20, interesting_freq+30, 2)
-        VDC_fine = []
-        #VDC = VDC + [GetLockinValue(pype, freq) for freq in fine_freqs]
-        VDC_fine = [GetLockinValue(pype, freq) for freq in fine_freqs]
-        #VDC_freqs += fine_freqs
-        #for freq,volt in zip(VDC_freqs, VDC):
-        #    print(str(freq) + ' MHz -> ' + str(volt) + ' V')
+        VDC_fine = GetVoltages(pype, fine_freqs)
+        #find global extrema
         min_index = VDC_fine.index(min(VDC_fine))
         max_index = VDC_fine.index(max(VDC_fine))
-        #minima = signal.argrelextrema(array(VDC_fine), less)[0]
-        #min_index = min((array(VDC_fine)[minima]<0)*minima)
-        #maxima = signal.argrelextrema(array(VDC_fine), less)[0]
-        #max_index = max((array(VDC_fine)[maxima]>0)*maxima)
+        dataset = zip(fine_freqs, VDC_fine)
+        #assert VDC_fine[min_index]*VDC_fine[max_index] < 0, 'zero_crossing'
+        crossing = where(diff(sign(VDC_fine)))[0][-1]
+        assert crossing, 'zero_crossing'
+
+        #zoom in on the zero crossing
+#        count = 20
+#        left = fine_freqs[min(min_index, max_index)]
+#        right = fine_freqs[max(min_index, max_index)]
+#        leftV = VDC_fine[fine_freqs.index(left)]
+#        rightV = VDC_fine[fine_freqs.index(right)]
+#        while count:
+#            freq = left - leftV*(right-left)/(rightV-leftV)
+#            voltage = GetVoltages(pype, freq)[0]
+#            if voltage*leftV<0:
+#                right = freq
+#                rightV = voltage
+#            elif voltage*rightV<0:
+#                left = freq
+#                leftV = voltage
+#            else:
+#                assert voltage, 'zero_crossing'
+#            dataset = sorted(dataset+[(freq, voltage)])
+#            count = (count-1)*((leftV*rightV)<0)*((right-left)>.1)
 
         #take some very finely spaced data for doing a fit
-        glmin = VDC_fine.index(max(VDC_fine))
-        glmax = VDC_fine.index(min(VDC_fine))
-        print('max and min are: ' + str(max(VDC_fine)) + ' ' + str(min(VDC_fine)))
-        firstpos = list(array(VDC_fine[glmin:glmax])<0).index(True)+glmin
-        very_fine_freqs = arange(fine_freqs[firstpos-1], fine_freqs[firstpos], 0.1)
+   #     firstpos = list(array(VDC_fine[glmin:glmax])<0).index(True)+glmin
+        very_fine_freqs = arange(fine_freqs[crossing]-.5, fine_freqs[crossing+1]+.5, 0.1)
+   #     very_fine_freqs = arange(fine_freqs[firstpos-1], fine_freqs[firstpos], 0.1)
         print('starting very fine grain frequency measurement')
-        VDC_very_fine = [GetLockinValue(pype, freq) for freq in very_fine_freqs]
+        VDC_very_fine = GetVoltages(pype, very_fine_freqs)#[GetLockinValue(pype, freq) for freq in very_fine_freqs]
 
         slope, intercept, r_value, p_value, std_err = stats.linregress(array(very_fine_freqs), array(VDC_very_fine))
         
@@ -139,5 +156,7 @@ if __name__ == "__main__":
 
     except AssertionError as e:
         if e[0] == 'interesting_freq':
-            print('No interesting frequencies found')
+            print('\nNo interesting frequencies found\n')
+        if e[0] == 'zero_crossing':
+            print('\nNo zero question \n')
         raise
