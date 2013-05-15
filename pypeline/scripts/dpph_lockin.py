75f96cd5fafcb1ab46a9b36db0ec3f8427ba208e
Ben LaRoque <benjamin.laroque@gmail.com>
2013-05-15 11:54:04 -0700

    local machines still need a sleep

--- pypeline/scripts/dpph_lockin.py
+++ pypeline/scripts/dpph_lockin.py
@@ -25,6 +25,7 @@ def GetLockinValue(interface, freq=25553.440, slptime=2):
     try:
         interface.Set('hf_cw_freq', freq).Wait()['result'] == 'ok'
         drip_resp = interface.Get('dpph_magphase').Wait()
+        sleep(slptime)
         magphase = [float(val) for val in drip_resp['final'].split(',')]
         return magphase[0]*sign(sin(magphase[1]*pi/180))
     except KeyError as keyname:
