4e7ebf7941eb8e0faf8f01e3499c75a677eff4e5
Ben LaRoque <benjamin.laroque@gmail.com>
2013-05-23 22:33:00 -0700

    some inheritance

--- pypeline/LogInterface.py
+++ pypeline/LogInterface.py
@@ -5,6 +5,7 @@
 # standard imports
 from time import sleep
 from uuid import uuid4
+from datetime import datetime
 
 # 3rd party imports
 
@@ -28,6 +29,35 @@ class _LogInterface:
             <conf_database> is the dripline configuration database (element of a couchdb Server object)
         '''
         self._log_database = log_database
+
+    def GetTimeSeries(self, sensor, start, stop):
+        '''
+            Retrieve data logged on CouchDB in a specified window and return it
+            as a numpy array.
+
+            Inputs:
+                <sensor> (string) is the sensor whose logged data you would
+                         like to retrieve
+                <start> (string) is the timestamp of the beginning of the log
+                        you would like to retrieve
+                <stop> (string) is the timestamp of the end of the log you
+                       would like to retrieve
+            Returns:
+                (timelist, valuelist, unitlist)
+        '''
+        timelist = []
+        valuelist = []
+        unitlist = []
+        for row in self._log_database.view('log_access/all_logged_data',
+                                          startkey=start, endkey=stop):
+            timestamp = datetime.strptime(row.value['timestamp_localstring'],
+                                          "%Y-%m-%d %H:%M:%S")
+            if row.value['sensor_name'] == sensor:
+                timelist.append(timestamp)
+                valuelist.append(float(row.value[
+                                 'calibrated_value'].split()[0]))
+                unitlist.append(str(row.value['calibrated_value'].split()[1]))
+        return (timelist, valuelist, unitlist)
 
     def LogValue(self, sensor, uncal_val, cal_val, timestamp):
         '''
