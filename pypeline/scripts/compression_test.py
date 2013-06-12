e01a9438d11e5c465175acfe652163c4ef40650f
Ben LaRoque <benjamin.laroque@gmail.com>
2013-06-12 16:55:42 -0700

    partial compression test

--- pypeline/scripts/compression_test.py
+++ pypeline/scripts/compression_test.py
@@ -5,8 +5,7 @@ from numpy import arange
 
 
 def compression_test(pype, power_start=-80, power_stop=-10, power_step=5,
-                     sweep_frequency=25500, lo_frequency=50,
-                     digitization_time=10):
+                     lo_frequency=50, digitization_time=50):
     '''
         Execute a compression test
 
@@ -15,17 +14,21 @@ def compression_test(pype, power_start=-80, power_stop=-10, power_step=5,
             <power_start>: low power for scan (in dBm)
             <power_stop>: high power for scan (in dBm)
             <power_step>: power step size (in dBm)
-            <sweep_frequency>: sweeper frequency(in MHz)
             <lo_frequency>: frequency at which to scan (in MHz)
             <digitization_time>: duration of mantis run (in ms)
     '''
+    sweep_frequency = lo_frequency + 24250
     pype.Set('hf_cw_freq', sweep_frequency)
     pype.Set('lo_cw_freq', lo_frequency)
     tempfile = '/data/thisisatempfileforcompressiontests.egg'
+    descrip = {'lo_frequency': lo_frequency}
+    rate = 200
+    mode = 1
     powers = []
     for power in arange(power_start, power_stop, power_step):
         pype.Set('hf_sweeper_power', power)
-        run_out = pype.Run(filename=tempfile, durration=digitization_time)
+        run_out = pype.Run(filename=tempfile, mode=mode, rate=rate,
+                           durration=digitization_time, description=descrip)
         # something to run powerline and get the power back
         # further processing of powerline output
         # something to append the power list
