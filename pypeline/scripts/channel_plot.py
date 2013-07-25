4f1f9b920f7ba9a4b97035030ac83bfd3e77f150
Ben LaRoque <benjamin.laroque@gmail.com>
2013-07-25 09:23:29 -0700

    improved plotting

--- pypeline/scripts/channel_plot.py
+++ pypeline/scripts/channel_plot.py
@@ -22,7 +22,7 @@ else:
 from datetime import datetime, timedelta
 
 #3rd party libs
-from numpy import arange, sin, cos, pi
+from numpy import arange, sin, cos, pi, array
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2TkAgg)
 from matplotlib.figure import Figure
@@ -93,7 +93,8 @@ class channel_plot:
         start_entry.bind('<KP_Enter>', self.Update, '+')
         start_entry.grid(row=4, column=2, columnspan=2)
         Checkbutton(self.toplevel, text='Hours ago',
-                    variable=self.relative_start_time).grid(row=5, column=2)
+                    variable=self.relative_start_time).grid(row=4, column=4,
+                                                            sticky='W')
 
         Label(self.toplevel, text='Stop Time').grid(row=6, column=1)
         stop_entry = Entry(self.toplevel, textvariable=self.stop_t)
@@ -101,7 +102,8 @@ class channel_plot:
         stop_entry.bind('<KP_Enter>', self.Update, '+')
         stop_entry.grid(row=6, column=2, columnspan=2)
         Checkbutton(self.toplevel, text='Now',
-                    variable=self.relative_stop_time).grid(row=7, column=2)
+                    variable=self.relative_stop_time).grid(row=6, column=4,
+                                                           sticky='W')
 
         Label(self.toplevel, text='Y limits (min-max)').grid(row=8, column=1)
         ymin = Entry(self.toplevel, textvariable=self.ymin)
@@ -132,11 +134,15 @@ class channel_plot:
         '''
         '''
         self.figure = Figure()
+        #self.figure.set_size_inches((4, 3), forward=True)
         self.figure.subplots_adjust(left=0.15, bottom=0.2)
-        self.subfigure = []
+        #self.subfigure = []
+        self.subfigure = self.figure.add_subplot(1,1,1)
         self.notebook = Notebook(self.toplevel)
-        self.notebook.grid(row=1, column=1, rowspan=3, columnspan=3,
-                           sticky='nsew')
+        self.notebook.grid(row=1, column=1, rowspan=3, columnspan=3, sticky='nsew')
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.toplevel)
+        self.canvas.show()
+        self.canvas.get_tk_widget().grid(row=0, column=0, rowspan=10)
 
     def _AddSubplot(self):
         '''
@@ -149,7 +155,7 @@ class channel_plot:
         self.plot_dicts['xunit'] = False
         self.plot_dicts[plotnum]['yname'] = StringVar(value='None')
         self.plot_dicts['yunit'] = False
-        self.subfigure.append(self.figure.add_subplot(1, 1, 1))
+        #self.subfigure.append(self.figure.add_subplot(1, 1, 1))
         Label(frame, text='X Channel').grid(row=0, column=0)
         Label(frame, text='Y Channel').grid(row=1, column=0)
         OptionMenu(frame, self.plot_dicts[plotnum]['xname'],
@@ -235,21 +241,29 @@ class channel_plot:
         self.status_var.set('updating')
         if tab == 'All':
             tab = range(len(self.notebook.tabs()))
-            self.subfigure[0].cla()
+            #self.subfigure.cla()
         elif isinstance(tab, int):
             tab = [tab]
         else:
             raise ValueError('tab should be "All" or an int')
         self._SetStart(event=None, isFirst=False)
         self._SetStop(event=None, isFirst=False)
+        self.subfigure.clear()
         #git dat gibblit grayvy
         for tabi in tab:
-            self._UpdateData(tab=tabi)
-            self._MakePlot(tab=tabi)
+            if tabi > len(self.subfigure.get_lines()):
+                print('wtf')
+            elif tabi == len(self.subfigure.get_lines()):
+                self._UpdateData(tab=tabi)
+                self._MakePlot(tab=tabi)
+            else:
+                self._UpdateExisting(tab=tabi)
+                #this is where I just update the line
         #dont stop git it git it
         self.figure.legends = []
-        self.figure.legend(*self.subfigure[0].get_legend_handles_labels())
+        self.figure.legend(*self.subfigure.get_legend_handles_labels())
         self.figure.legends[0].draggable(True)
+        self.canvas.draw()
         self.status_var.set('updated at: ' +
                             datetime.now().strftime(self._formatstr))
         if (self.continuous_updates.get() and self.relative_stop_time.get() and
@@ -296,38 +310,79 @@ class channel_plot:
         except AssertionError as e:
             print('*'*60, '\n the', e[0], 'do not match the 0th line', '*'*60)
 
+    def _UpdateExisting(self, tab=0):
+        '''
+        '''
+        try:
+            yname = self.plot_dicts[tab]['yname'].get()
+            ychdat = self.pype.GetTimeSeries(yname, self.time_interval[0],
+                                             self.time_interval[1])
+            if self.plot_dicts[tab]['xname'].get() == 'time':
+                xchdat = (ychdat[0], ychdat[0], 'time' * len(ychdat[0]))
+            else:
+                xname = self.plot_dicts[tab]['xname'].get()
+                xchdat = self.pype.GetTimeSeries(xname, self.time_interval[0],
+                                                 self.time_interval[1])
+            if tab > 0 and ychdat[0]:
+                assert xchdat[2][0] == self.plot_dicts['xunit'], 'x units'
+                assert ychdat[2][0] == self.plot_dicts['yunit'], 'y units'
+
+            self.xdata = []
+            self.ydata = []
+            if ychdat[0]:
+                for tx, x in zip(xchdat[0], xchdat[1]):
+                    xtmp = False
+                    ytmp = False
+                    dt = timedelta(seconds=60)
+                    for ty, y in zip(ychdat[0], ychdat[1]):
+                        if abs(ty - tx) < dt:
+                            dt = abs(ty - tx)
+                            xtmp = x
+                            ytmp = y
+                    if xtmp and ytmp:
+                        self.xdata.append(xtmp)
+                        self.ydata.append(ytmp)
+                [self.xdata, self.ydata] = zip(*sorted(zip(self.xdata,
+                                                           self.ydata)))
+                self.plot_dicts['xunit'] = xchdat[2][0]
+                self.plot_dicts['yunit'] = ychdat[2][0]
+            this_line = self.subfigure.get_lines()[tab]
+            this_line.set_xdata(array(self.xdata))
+            this_line.set_ydata(array(self.ydata))
+
+        except AssertionError as e:
+            print('*'*60, '\n the', e[0], 'do not match the 0th line', '*'*60)
+
     def _MakePlot(self, tab=0):
         '''
         '''
+        #self.figure.get_axes()[0].clear()
         if self.plot_dicts[tab]['xname'].get() == 'time':
-            self.subfigure[tab].plot_date(self.xdata, self.ydata, fmt='o-',
+            self.subfigure.plot_date(self.xdata, self.ydata, fmt='o-',
                                           label=self.plot_dicts[tab]['yname'].get())
-            self.subfigure[tab].set_xticklabels(
-                self.subfigure[tab].get_xticklabels(), rotation=-45)
-            self.subfigure[tab].xaxis.set_major_formatter(dates.DateFormatter(
+            self.subfigure.set_xticklabels(self.subfigure.get_xticklabels(),
+                                           rotation=-45)
+            self.subfigure.xaxis.set_major_formatter(dates.DateFormatter(
                 "%m/%d %H:%M"))
         else:
-            self.subfigure[tab].plot(self.xdata, self.ydata,
-                                     label=self.plot_dicts[tab]['yname'].get())
-        self.subfigure[tab].set_title(self.plot_dicts[tab]['yname'].get() +
-                                      ' vs ' +
-                                      self.plot_dicts[tab]['xname'].get() +
-                                      '\n from ' + self.time_interval[0] +
-                                      ' to ' + self.time_interval[1])
+            self.subfigure.plot(self.xdata, self.ydata,
+                                label=self.plot_dicts[tab]['yname'].get())
+        self.subfigure.set_title(self.plot_dicts[tab]['yname'].get() +
+                                 ' vs ' +
+                                 self.plot_dicts[tab]['xname'].get() +
+                                 '\n from ' + self.time_interval[0] +
+                                 ' to ' + self.time_interval[1])
         xname = self.plot_dicts[tab]['xname'].get().replace('_', ' ')
         xunit = '[' + str(self.plot_dicts['xunit']) + ']'
-        self.subfigure[tab].set_xlabel(xname + ' ' + xunit)
+        self.subfigure.set_xlabel(xname + ' ' + xunit)
         yname = self.plot_dicts[tab]['yname'].get().replace('_', ' ')
         yunit = '[' + str(self.plot_dicts['yunit']) + ']'
-        self.subfigure[tab].set_ylabel(yname + ' ' + yunit)
+        self.subfigure.set_ylabel(yname + ' ' + yunit)
         #self.subfigure[tab].ticklabel_format(useOffset=False)
         if self.ManualLimits.get():
-            self.subfigure[tab].set_ylim(bottom=self.ymin.get(),
-                                         top=self.ymax.get())
+            self.subfigure.set_ylim(bottom=self.ymin.get(), top=self.ymax.get())
 
-        self.canvas = FigureCanvasTkAgg(self.figure, master=self.toplevel)
-        self.canvas.show()
-        self.canvas.get_tk_widget().grid(row=0, column=0, rowspan=10)
+        #self.canvas.draw()
 
     def _PlotGasLines(self):
         '''
