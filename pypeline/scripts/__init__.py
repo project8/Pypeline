9ea58b2a403d220b4a9f632ba2f889c58bf2b1b2
Ben LaRoque <benjamin.laroque@gmail.com>
2013-07-25 15:15:35 -0700

    new gui class for sweeper based dpph

--- pypeline/scripts/__init__.py
+++ pypeline/scripts/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from .check_pulse import check_pulse
-from .dpph import measure_dpph
+from .dpph import measure_dpph, swept_dpph_measurement
 from .run_mantis import run_mantis
 from .channel_plot import channel_plot
 from .update_pypeline_confDB import update_pypeline_confDB
