815e3e8723d4d3516c1d5ffcdfe77f3a236b0579
could be anyone <project8@myrna>
2013-10-19 09:44:31 -0700

    need to rethink import of subsubmodule

--- pypeline/__init__.py
+++ pypeline/__init__.py
@@ -5,10 +5,10 @@
     There is currently support for use with both python 2.7 and python 3.2
     (earlier probably works also).
 '''
-from __future__ import print_function
+from __future__ import print_function, absolute_import
 from os import environ
 
 from .DripInterface import DripInterface
 from .DripResponse import DripResponse
 from .PypelineErrors import NoResponseError
-from . import scripts
+#import .scripts
