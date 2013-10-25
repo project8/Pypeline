ca22e578c4250ff689c0aa333c42d2a848b521cf
Ben LaRoque <benjamin.laroque@gmail.com>
2013-10-25 15:32:55 -0700

    creating interface class

---
+++ pid_control/pid_interface.py
@@ -0,0 +1,21 @@
+from __future__ import print_function, absolute_import
+
+# Standard
+
+# 3rd Party
+
+# Local
+
+class pid_interface
+    '''
+    '''
+
+    def __init__(self):
+        '''
+        '''
+        pass
+
+    def AddController():
+        '''
+        '''
+        pass
