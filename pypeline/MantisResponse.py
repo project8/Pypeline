f010aaf785817b02dd08f36def8381fe91c5e660
Ben LaRoque <benjamin.laroque@gmail.com>
2013-12-23 13:15:32 -0800

    beginnings of mantis interface

---
+++ pypeline/MantisResponse.py
@@ -0,0 +1,33 @@
+'''
+    Mantis uses a client/server model to control signal digitization systems. Being purely software, the interface to that client should be taken care of directly by pypeline, without a dripline translation layer.
+
+    The DripResponse object does not apply but equivalent behavior is desired.
+'''
+
+from __future__ import print_function, absolute_import
+
+class MantisResponse():
+    '''
+    '''
+
+
+    def __init__(self, kwargs={}):
+        '''
+            
+        '''
+        pass
+
+    def Update(self):
+        '''
+        '''
+        pass
+
+    def Waiting(self):
+        '''
+        '''
+        pass
+
+    def Wait(self):
+        '''
+        '''
+        pass
