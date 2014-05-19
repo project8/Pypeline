1f467d296772cd34ee72a0d8b9ec1a33c1206d1d
Ben LaRoque <benjamin.laroque@gmail.com>
2014-05-19 13:20:36 -0700

    version number added

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'Pypeline',
+    version = open("VERSION").read().strip().replace(' ','.'),
     packages = ['pypeline','pypeline/scripts','pypeline/scripts/dpph', 'pypeline/scripts/take_data', 'pid_control'],
     scripts = ['gpypeline']
 )
