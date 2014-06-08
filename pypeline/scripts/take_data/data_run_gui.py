97c18f3e111d40c20a87353d5b3b16adf3e49ac8
Ben LaRoque <benjamin.laroque@gmail.com>
2014-06-07 18:40:10 -0700

    changing the data taking gui layout further

--- pypeline/scripts/take_data/data_run_gui.py
+++ pypeline/scripts/take_data/data_run_gui.py
@@ -69,7 +69,7 @@ class take_data:
         Checkbutton(self.toplevel, text="Extend if Exists",
                     variable=self.extend_runVar).grid(row=row, column=3)
         row += 1
-        Label(self.toplevel, text="(raid location)").grid(row=row, column=0)
+        Label(self.toplevel, text="(raid location)").grid(row=row, column=0, sticky='E')
         Label(self.toplevel, text="(run type)").grid(row=row, column=1)
         Label(self.toplevel, text="(run tag)").grid(row=row, column=2)
         row += 1
@@ -83,31 +83,35 @@ class take_data:
         Entry(self.toplevel, textvariable=self.num_sequencesVar).grid(row=row,
                                                                       column=1)
         row += 1
-        Label(self.toplevel, text='Sequence Spacing').grid(row=row, column=0)
+        Label(self.toplevel, text='Delay Between').grid(row=row, column=0)
         Entry(self.toplevel, textvariable=self.sequence_spacingVar).grid(row=row, column=1)
         Label(self.toplevel, text='[s]').grid(row=row, column=2, sticky='W')
         row += 1
-
-        Label(self.toplevel, text='Sequence Length').grid(row=row, column=0)
-        Entry(self.toplevel, textvariable=self.len_sequenceVar).grid(row=row,
-                                                                     column=1)
-        Label(self.toplevel, text='[ms]').grid(row=row, column=2, sticky='W')
-        row += 1
-
-
         builtins_list = ['default_run', 'noise_analysis_run']
+        self.conf_filename.set(builtins_list[0])
         Button(self.toplevel, text='Load Builtin Run Def',
                command=self._GetParamFuncs).grid(row=row, column=0)
         OptionMenu(self.toplevel, self.conf_filename, *builtins_list).grid(
             row=row, column=1)
         self.conf_filename.set(builtins_list[0])
-
         row += 1
-
         Label(self.toplevel, text='Load Custom Run Def').grid(row=row,
                                                               column=0)
         Button(self.toplevel, text="find file", command=self._ParamFuncFile
                ).grid(row=row, column=1)
+        row += 1
+
+        # Mantis Settings
+        Label(self.toplevel, text='-'*20+'Mantis Settings'+'-'*20).grid(row=row,
+                                                        columnspan=3, column=0)
+        row += 1
+        Label(self.toplevel, text='(Empty fields use default values)').grid(row=row,
+                                                        columnspan=3, column=0)
+        row += 1
+        Label(self.toplevel, text='Digitization Time').grid(row=row, column=0)
+        Entry(self.toplevel, textvariable=self.len_sequenceVar).grid(row=row,
+                                                                     column=1)
+        Label(self.toplevel, text='[ms]').grid(row=row, column=2, sticky='W')
         row += 1
 
         Button(self.toplevel, text="Start Run", command=self.DoRun
