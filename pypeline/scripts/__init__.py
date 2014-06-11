0884e2c571bc5ce1a7ff59784fbd206a8683c7f4
Ben LaRoque <benjamin.laroque@gmail.com>
2014-06-11 13:41:02 -0700

    forgot the update to the init

--- pypeline/scripts/__init__.py
+++ pypeline/scripts/__init__.py
@@ -2,8 +2,6 @@ from __future__ import absolute_import
 
 from .check_pulse import check_pulse
 from .dpph import *
-from .run_mantis import run_mantis
 from .channel_plot import channel_plot
-from .update_pypeline_confDB import update_pypeline_confDB
 from .take_data import take_data
 from .compression_test import compression_test
