a5be3205acf5a57e2d092b06f3773d7cf361a724
Ben LaRoque <benjamin.laroque@gmail.com>
2014-06-11 13:42:22 -0700

    also prune some other imports

--- pypeline/scripts/__init__.py
+++ pypeline/scripts/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
-from .check_pulse import check_pulse
+#from .check_pulse import check_pulse
 from .dpph import *
 from .channel_plot import channel_plot
-from .take_data import take_data
-from .compression_test import compression_test
+#from .take_data import take_data
+#from .compression_test import compression_test
