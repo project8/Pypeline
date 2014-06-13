00fe954874c6a5fb8ffd6be9ed7af245dd0c8ad3
Luiz de Viveiros <ldeviveiros@physics.ucsb.edu>
2014-06-13 11:30:31 -0700

    night watcher now restarts the run

--- bin/night_watcher.sh
+++ bin/night_watcher.sh
@@ -3,6 +3,7 @@
 # Watch that the night_run is working
 
 RUNDATE=20140613
+SESSION=python_night_run
 
 if [[ -n $(find /data/runs/$RUNDATE -mmin -10) ]]; then
 	ls /data/runs/$RUNDATE
@@ -23,10 +24,17 @@ else
 	echo restarting Ignatius
 	ssh root@ignatius 'shutdown -h now'
 	sleep 120
+	echo Wake up ignatius
 	wol 00:25:90:15:2f:b3
 	sleep 180
+	echo Start DAQ
 	ssh ignatius '~/Code/AntFarm/start_daq.sh'
 	sleep 60
-
+	echo Start Acquisition
+	tmux attach-session -d -s ${SESSION}
+	tmux select-pane -t ${SESSION}:1.0
+	tmux send-keys "rate_vs_time_study ~/night_run.json" C-m
+	echo Everything should be working now
+	sleep 60
 fi
 
